a914ee7dfede1c063c0c2b36b12a09ea5401221b
Ekram17 <153009137+Ekram17@users.noreply.github.com>
2024-04-22 10:31:17 -0700

    host update

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -7,12 +7,16 @@ const mysql2 = require('mysql2');
 var dbConnection= mysql2.createPool({
     user:process.env.user,
     database:process.env.DATABASE,
-    host:"82.197.80.207",
+    host:"193.203.166.19",
+
     port: 3306,
     password:process.env.PASSWORD,
     connectionLimit:10
 });
 console.log(process.env.JWT_SECRET)
+
+
+
 
 // dbConnection.execute("select 'test' ",(err,result)=>{
 //     if(err)
